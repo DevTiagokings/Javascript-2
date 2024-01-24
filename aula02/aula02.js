d78d70407c64e58be355ae141fbdacf64ff4277a
Tiago Gomes do Nascimento <133234836+DevTiagokings@users.noreply.github.com>
2024-01-24 07:16:19 -0300

    Atualizações de Aulas

---
+++ aula02/aula02.js
@@ -0,0 +1,7 @@
+"use strict" //Modo estrito 
+
+nome="Bruno"
+/*let nome="Bruno"*/
+console.log("CFC Cursos")
+console.log(nome)
+console.log("nome:" + nome)
---
+++ aula03/aula3.js
@@ -0,0 +1,14 @@
+"use strict"
+
+function teste(){
+    let nome="Bruno"
+    if(true){
+        console.log("dentro de teste" + nome)
+    }
+    console.log("dentro de teste:" + nome)
+}
+
+
+teste()
+
+console.log("Fora de teste:" + nome)
