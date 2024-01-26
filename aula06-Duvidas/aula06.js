0147c4fe174172d98a5c9540f2fd8dccaac1aefe
Tiago Gomes do Nascimento <133234836+DevTiagokings@users.noreply.github.com>
2024-01-26 08:11:59 -0300

    Retirada de dúvidas/Exclusão dos html

---
+++ aula06-Duvidas/aula06.js
@@ -0,0 +1,30 @@
+/*let n1=1
+let n2="1"
+
+let v1={nome:"Bruno"}
+let v2=v1
+
+console.log(v1===v2)*/
+
+/*let nome=prompt("Digite o seu nome")
+
+ console.log(nome)*/
+
+ /*let n1=1
+ let n2=2
+
+ console.log(n1!=n2)*/
+
+ let nome="bruno"
+
+ {
+    let siglacurso="js"
+    console.log(siglacurso)
+ }
+
+ const curso=()=>{
+    let curso="javascript"
+    console.log(curso)
+ }
+
+ curso()
