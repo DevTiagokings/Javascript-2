ae712468509fdf83f5f8ef503d6f467afaae376c
Tiago Gomes do Nascimento <133234836+DevTiagokings@users.noreply.github.com>
2024-01-30 08:59:08 -0300

    Operadores lógicos

--- aula07-operadorlóg/aula07.js
+++ aula07-operadorlóg/aula07.js
@@ -1,18 +1,20 @@
 //Operadores lógicos\\
 // && -> and e
-// || -> not não
+// || -> Or ou
+// ! -> not não
 
-//Resultado dos operadores lógicos será verdadeiro ou falso
-
-let n1,n2,n3,n4
+let n1, n2, n3, n4
 n1=10
 n2=5
 n3=15
 n4=2
 
-if((n1>n2) && (n3<n4) ){
-    console.log("Verdadeiro")
-}else{
+if((n1>n2) || (n3>n4)){
+    console.log("verdadeiro")
+} else{
     console.log("falso")
 }
+//Resultado dos operadores lógicos será verdadeiro ou falso
+
+
 //console.log(!((n1>n2)||(n1>n3)))
