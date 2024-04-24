626e333550eefaf68cb77d358fa72db77a1845a9
Tiago Gomes <Gomes.gt001@gmail.com>
2024-04-24 12:22:28 -0300

    revisão aula04

--- aula04-opermatemá/aula04.js
+++ aula04-opermatemá/aula04.js
@@ -1,15 +1,9 @@
-/*Formas de declarar diferentes varíaveis 
-let num1
-let num2
-
-let num1, num2*/
-
-let num1=0
-let num2=0
-let res=0
+/*Formas de declarar diferentes varíaveis*/ 
+/*let num1, num2=0*/
+let num1=0, num2=0,res=0
 
 num1=10
-num1+=5 
+num1*=2 //mu,1++ num1=num1+1
 
 console.log(num1)
 
